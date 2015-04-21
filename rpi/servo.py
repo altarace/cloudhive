4f029f246e275a25bac3ac0cd33baefb68f8bb3b
altarace <altarace@aol.com>
2015-04-21 00:00:22 -0400

    fake finger for overdrive wifi modem
    sprint is kicking me off during peak hours unless I press on the
    overdrive modem

---
+++ rpi/servo.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(11,GPIO.OUT)
+
+freq = 50
+pwm= GPIO.PWM(11,freq)
+
+leftp=0.75
+rightp = 2.5
+midp = (rightp - leftp)/2 +leftp
+mspercycle = 1000/freq
+dutycyup = midp*100/mspercycle
+dutydown = 2.3*100/mspercycle
+pwm.start(dutycyup)
+time.sleep(.5)
+pwm.start(dutydown)
+time.sleep(.5)
+pwm.start(dutycyup)
+time.sleep(.5)
+pwm.stop()
+GPIO.cleanup()
